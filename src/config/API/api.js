a417a35623c635445fc9587ecb85de8d31a0dfce
ducnvidiak <ducnivival@gmail.com>
2025-02-21 16:35:20 +0700

    fix api endpoint token for compatible with backend and postman

--- src/config/API/api.js
+++ src/config/API/api.js
@@ -1,29 +1,28 @@
 import axios from "axios";
 
 export const API = axios.create({
-    baseURL: `http://localhost:8080/`,
+  baseURL: `http://localhost:8080/`,
 });
 
 export const APIPrivate = axios.create({
-    baseURL: `http://localhost:8080/`,
-    withCredentials: true,
+  baseURL: `http://localhost:8080/`,
+  withCredentials: true,
 });
 
 // ✅ Get state dynamically inside interceptor
 APIPrivate.interceptors.request.use(
-    async (config) => {
-        // const storeModule = await import("../../redux/store"); // ✅ Dùng dynamic import
-        // const store = storeModule.default;
-        // const token = store.getState().auth.token;
+  async (config) => {
+    // const storeModule = await import("../../redux/store"); // ✅ Dùng dynamic import
+    // const store = storeModule.default;
+    // const token = store.getState().auth.token;
 
-        const token = localStorage.getItem("token");
-        console.log("Token before request: ", token);
+    const token = localStorage.getItem("token");
+    console.log("Token before request: ", token);
 
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-        return config;
-    },
-    (error) => Promise.reject(error)
+    if (token) {
+      config.headers.Authorization = `Bearer Token ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
 );
-
