9042d6f3b9b5749f8bfea7470f654d924638f011
Thợ săn bé trai <kthkun@gmail.com>
2025-03-04 14:45:21 +0700

    requesttagNames

--- src/screens/request/RequestDetailScreen.jsx
+++ src/screens/request/RequestDetailScreen.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { fetchRequestById } from "../../redux/request/requestSlice";
+import { fetchTags } from "../../redux/tag/tagSlice";
 import LoadingModal from "../../components/LoadingModal";
 import { Typography } from "antd";
 
@@ -12,12 +13,19 @@ const RequestDetailScreen = () => {
     const dispatch = useDispatch();
     const loading = useSelector((state) => state.request.loading);
     const request = useSelector((state) => state.request.currentRequest);
+    const tags = useSelector((state) => state.tag.tags);
 
     useEffect(() => {
         dispatch(fetchRequestById(id));
+        dispatch(fetchTags());
     }, [dispatch, id]);
 
     if (loading) return <LoadingModal />;
+
+    const tagNames = request.tagIds.map((tagId) => {
+        const tag = tags.find(tag => tag.id === tagId);
+        return tag ? tag.name : tagId;
+    });
 
     return (
         <div className="request-detail">
@@ -27,7 +35,7 @@ const RequestDetailScreen = () => {
             <Paragraph><strong>Email:</strong> {request.email}</Paragraph>
             <Paragraph><strong>Location:</strong> {request.location}</Paragraph>
             <Paragraph><strong>Category:</strong> {request.categoryId}</Paragraph>
-            <Paragraph><strong>Tags:</strong> {request.tagIds.join(", ")}</Paragraph>
+            <Paragraph><strong>Tags:</strong> {tagNames.join(", ")}</Paragraph>
             <Paragraph><strong>Status:</strong> {request.status}</Paragraph>
         </div>
     );
