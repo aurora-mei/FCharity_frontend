885f59cc9f50c640fa7cad14257fb236dfc99576
aurora-mei <huyencttde180614@fpt.edu.vn>
2025-04-19 14:09:19 +0700

    fix up file

--- src/containers/HomeContainer/HomeContainer.jsx
+++ src/containers/HomeContainer/HomeContainer.jsx
@@ -9,9 +9,11 @@ import NotingGreen from "../../components/NotingGreen/NotingGreen";
 import RequestActiveCarousel from "../../components/RequestActiveCarousel/RequestActiveCarousel";
 import { fetchProjectsThunk } from '../../redux/project/projectSlice';
 import { useEffect } from 'react';
-import { Flex } from 'antd';
+import { Flex,Upload,Button,message  } from 'antd';
 import { useDispatch, useSelector } from 'react-redux';
 import { useTranslation, Trans } from 'react-i18next';
+import {uploadFileMedia} from '../../redux/helper/helperSlice';
+import { UploadOutlined } from '@ant-design/icons';
 
 const HomeContainer = () => {
     const projects = useSelector(state => state.project.projects);
@@ -20,11 +22,46 @@ const HomeContainer = () => {
         dispatch(fetchProjectsThunk());
     }, [dispatch]);
     const { t, i18n } = useTranslation();
+    const handleImageChange = async ({ fileList }) => {
+        if (fileList.length === 0) return; // Nếu danh sách trống, không làm gì
+    
+        const latestFile = fileList[fileList.length - 1];
+    
+        try {
+          const response = await dispatch(
+            uploadFileMedia({
+              file: latestFile.originFileObj,
+              folderName: "images",
+              resourceType:"raw"
+            })
+          ).unwrap();
+          console.log("response", response);
+          latestFile.url = response;
+          message.success(`Uploaded ${latestFile.name}`);
+        } catch (error) {
+          console.error("Error uploading image:", error);
+          message.error(`Upload failed for ${latestFile.name}`);
+        }
+    
+      };
     return (
         <div> {/* Bỏ Flex ở đây nếu không cần thiết cho toàn bộ */}
             <Flex vertical={true} className='home-upper'>
                 <Banner />
                 <Noting />
+                <Upload
+                  multiple
+                  listType="picture"
+                  beforeUpload={() => false} // Không upload ngay, chờ xử lý thủ công
+                  beforeRemove={() => false}
+                  onChange={handleImageChange} // Xử lý khi chọn file
+                >
+                  <Button
+                    icon={<UploadOutlined />}
+                  >
+                    Click to Upload
+                  </Button>
+                </Upload>
                 <Flex vertical={true} gap='4rem' style={{ margin: '4rem 10rem' }}>
                     <RequestActiveCarousel />
                     <FundraiserBoard projects={projects} />
--- src/redux/helper/helperApi.js
+++ src/redux/helper/helperApi.js
@@ -21,7 +21,7 @@ const uploadFile = async ({ file, folderName = "default-folder" }) => {
   if (resourceType === "video") {
     uploadUrl = CLOUDINARY_URL.replace("/image/", "/video/");
   } else if (resourceType === "raw") {
-    uploadUrl = CLOUDINARY_URL.replace("/image/", "/raw");
+    uploadUrl = CLOUDINARY_URL.replace("/image/", "/raw/");
   }
 
   const isVideo = file.type.startsWith("video/");
@@ -49,17 +49,13 @@ const uploadFileMedia = async ({ file, folderName = "default-folder",resourceTyp
   formData.append("upload_preset", PRESET_NAME);
   formData.append("folder", folderName); // 🌟 Thêm folder tùy chỉnh
   formData.append("resource_type", resourceType);
-
+ let uploadUrl = CLOUDINARY_URL;
   if (resourceType === "video") {
     uploadUrl = CLOUDINARY_URL.replace("/image/", "/video/");
   } else if (resourceType === "raw") {
-    uploadUrl = CLOUDINARY_URL.replace("/image/", "/raw");
+    uploadUrl = CLOUDINARY_URL.replace("/image/", "/raw/");
   }
 
-  const isVideo = file.type.startsWith("video/");
-  const uploadUrl = isVideo
-    ? `${CLOUDINARY_URL.replace("/image/", "/video/")}`
-    : CLOUDINARY_URL;
   console.log("up url", uploadUrl);
   try {
     const res = await axios.post(uploadUrl, formData, {
